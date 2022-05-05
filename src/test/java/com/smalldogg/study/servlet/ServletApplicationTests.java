9795658d7e4193c733adc10cb12780358d726e6f
small-dogg <sy920112@naver.com>
2022-05-05 10:30:52 +0900

    chore: init commit

---
+++ src/test/java/com/smalldogg/study/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smalldogg.study.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
